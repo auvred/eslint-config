1501a8616669ddeba03df00711e02a3b63f0a463
auvred <aauvred@gmail.com>
2023-12-02 13:55:55 +0000

    fix: update ts/ban-ts-comment options

--- index.js
+++ index.js
@@ -168,11 +168,15 @@ const config = {
     '@typescript-eslint/ban-ts-comment': [
       'error',
       {
-        'ts-expect-error': 'allow-with-description',
-        'ts-ignore': 'allow-with-description',
-        'ts-nocheck': 'allow-with-description',
+        'ts-expect-error': {
+          descriptionFormat: '^ - .+$',
+        },
+        'ts-ignore': true,
+        'ts-nocheck': {
+          descriptionFormat: '^ - .+$',
+        },
         'ts-check': false,
-        minimumDescriptionLength: 3,
+        minimumDescriptionLength: 7,
       },
     ],
   },
