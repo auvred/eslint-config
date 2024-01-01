4a45ccbbd135e55ed5d4b0d04bfc3f04dfc5b5e2
auvred <aauvred@gmail.com>
2024-01-01 17:58:37 +0000

    chore: lint

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig([{
-  entry: ['src/index.ts'],
-  format: 'esm',
-  clean: true,
-  dts: true,
-  shims: true,
-}, {
-  entry: ['src/plugin/workers/prettier-format.ts'],
-  outDir: 'dist/workers',
-  format: 'esm',
-  shims: true,
-}])
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    format: 'esm',
+    clean: true,
+    dts: true,
+    shims: true,
+  },
+  {
+    entry: ['src/plugin/workers/prettier-format.ts'],
+    outDir: 'dist/workers',
+    format: 'esm',
+    shims: true,
+  },
+])
