5d64c0cdf8aca92c9945436ef348c0f399e4de65
auvred <aauvred@gmail.com>
2024-01-02 12:58:43 +0000

    fix: handle prettier format errors

--- src/plugin/rules/prettier.ts
+++ src/plugin/rules/prettier.ts
@@ -6,7 +6,7 @@ import { messages, reportDifferences } from 'eslint-formatting-reporter'
 
 import { getPrettierFormatSyncFn } from '../workers'
 
-import type { Rule } from 'eslint'
+import type { AST, Rule } from 'eslint'
 
 const rule: Rule.RuleModule = {
   meta: {
@@ -33,13 +33,38 @@ const rule: Rule.RuleModule = {
       Program(): void {
         const sourceCode = context.sourceCode.text
 
-        const formatted = format({
-          code: sourceCode,
-          filepath: context.filename,
-          prettierOptions: context.options[0] || {},
-        })
+        try {
+          const formatted = format({
+            code: sourceCode,
+            filepath: context.filename,
+            prettierOptions: context.options[0] || {},
+          })
 
-        reportDifferences(context, sourceCode, formatted)
+          reportDifferences(context, sourceCode, formatted)
+        } catch (error) {
+          // Source: https://github.com/prettier/eslint-plugin-prettier/blob/f9857187b4d43d2f0d20104a8c94eb4abbd44725/eslint-plugin-prettier.js#L209-L233
+
+          if (!(error instanceof SyntaxError)) {
+            throw error
+          }
+
+          let message = `Parsing error: ${error.message}`
+
+          // Prettier's message contains a codeframe style preview of the
+          // invalid code and the line/column at which the error occurred.
+          // ESLint shows those pieces of information elsewhere already so
+          // remove them from the message
+          if ('codeFrame' in error && error.codeFrame) {
+            message = message.replace(`\n${error.codeFrame}`, '')
+          }
+          if ('loc' in error && error.loc) {
+            message = message.replace(/ \(\d+:\d+\)$/, '')
+            context.report({
+              message,
+              loc: error.loc as AST.SourceLocation,
+            })
+          }
+        }
       },
     }
   },
